ea636cdceb48b66b12a05a57f344eb81b636ba40
Jorge R. <jorge@programmer.net>
2024-02-26 15:50:18 -0700

    add data

---
+++ src/data/data.ts
@@ -0,0 +1,32 @@
+const cartItems = [
+  {
+    id: 'rec1JZlfCIBOPdcT2',
+    title: 'Juan Cordero',
+    price: '80.00',
+    img: 'https://res.cloudinary.com/dilteafq8/image/upload/v1705090670/thumbnail_juancordero_6e73730dcb.jpg',
+    amount: 1,
+  },
+  {
+    id: 'recB6qcHPxb62YJ75',
+    title: 'Bohemia Pilsner',
+    price: '19.00',
+    img: 'https://res.cloudinary.com/dilteafq8/image/upload/v1705090357/thumbnail_bohemia_5bda4c605c.jpg',
+    amount: 1,
+  },
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Goose IPA',
+    price: '48.00',
+    img: 'https://res.cloudinary.com/dilteafq8/image/upload/v1705089811/thumbnail_gooseipa_e6ed93b2e5.jpg',
+    amount: 1,
+  },
+  {
+    id: 'recwTo160XST3PIoW',
+    title: 'Lupulosa',
+    price: '80.00',
+    img: 'https://res.cloudinary.com/dilteafq8/image/upload/v1705087362/thumbnail_lupulosa_eee7d9fb9a.jpg',
+    amount: 1,
+  },
+];
+
+export default cartItems;
